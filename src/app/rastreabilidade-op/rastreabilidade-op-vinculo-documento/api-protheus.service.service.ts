6cc37225dd5a50a39adec6a473cb88ccddd6d4ba
Kleber Santos <kleberrogersantos@gmail.com>
2026-01-27 17:08:44 -0300

    Implementações da Rastreabilidade da OP

--- src/app/rastreabilidade-op/rastreabilidade-op-vinculo-documento/api-protheus.service.service.ts
+++ src/app/rastreabilidade-op/rastreabilidade-op-vinculo-documento/api-protheus.service.service.ts
@@ -3,27 +3,21 @@ import api from '../../http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiProtheusService {
   readonly url = sessionStorage.getItem('mrHost') + 'minhasrotinas';
-
 
   mrCodigoEmpresa = sessionStorage.getItem('mrCodigoEmpresa');
   mrNomeEmpresa = sessionStorage.getItem('mrNomeEmpresa');
   mrDataBase = sessionStorage.getItem('mrDataBase');
   mrUsuario = sessionStorage.getItem('mrUsuario');
 
-
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
 
   public async getLinkedOrders(): Promise<any[]> {
-
     try {
-
       const httpOptions = {
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
@@ -32,19 +26,51 @@ export class ApiProtheusService {
         }),
       };
 
-      let filial = this.mrCodigoEmpresa
-      const response = await firstValueFrom(this.http.get<{ linkedProductionOrders: any[] }>(`${this.url}/vinculos/ops-vinculadas?branch=${filial}`, httpOptions));
+      let filial = this.mrCodigoEmpresa;
+      const response = await firstValueFrom(
+        this.http.get<{ linkedProductionOrders: any[] }>(
+          `${this.url}/vinculos/ops-vinculadas?branch=${filial}`,
+          httpOptions,
+        ),
+      );
       console.warn(`Resposta -->`);
       console.warn(response);
       return response.linkedProductionOrders;
     } catch (error) {
-      throw (error);
+      throw error;
     }
   }
 
-  public async getAvailableDocuments(branch: string, productionId: string): Promise<any[]> {
+  public async getCustomers(): Promise<any[]> {
     try {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Basic ' + btoa('Admin:teste@123'),
+          Accept: '*/*',
+        }),
+      };
 
+      let filial = this.mrCodigoEmpresa;
+      const response = await firstValueFrom(
+        this.http.get<{ customers: any[] }>(
+          `${this.url}/associations/customers/with-production-orders`,
+          httpOptions,
+        ),
+      );
+      console.warn(`Resposta -->`);
+      console.warn(response);
+      return response.customers;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async getAvailableDocuments(
+    branch: string,
+    productionId: string,
+  ): Promise<any[]> {
+    try {
       const httpOptions = {
         headers: new HttpHeaders({
           'Content-Type': 'application/json',
@@ -56,15 +82,41 @@ export class ApiProtheusService {
       const response = await firstValueFrom(
         this.http.get<{ availableDocuments: any[] }>(
           `${this.url}/vinculos/available-documents?branch=${branch}&productionId=${productionId}`,
-          httpOptions
-        )
+          httpOptions,
+        ),
       );
       console.warn(`Resposta -->`);
       console.warn(response);
       return response.availableDocuments;
     } catch (error) {
-      throw (error);
+      throw error;
     }
   }
 
+  public async getAvailableProductionsOrders(
+    customerCode: string,
+    customerBranch: string,
+  ): Promise<any[]> {
+    try {
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Basic ' + btoa('Admin:teste@123'),
+          Accept: '*/*',
+        }),
+      };
+
+      const response = await firstValueFrom(
+        this.http.get<{ productionOrders: any[] }>(
+          `${this.url}/associations/productions-orders/availables?customerCode=${customerCode}&customerBranch=${customerBranch}`,
+          httpOptions,
+        ),
+      );
+      console.warn(`Resposta -->`);
+      console.warn(response);
+      return response.productionOrders;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
--- src/app/rastreabilidade-op/rastreabilidade-op-vinculo-documento/rastreabilidade-op-vinculo-documento.component.ts
+++ src/app/rastreabilidade-op/rastreabilidade-op-vinculo-documento/rastreabilidade-op-vinculo-documento.component.ts
@@ -2,10 +2,26 @@ import { NgStyle } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { PoButtonModule, PoComboFilterMode, PoDividerModule, PoFieldModule, PoImageModule, PoLoadingModule, PoMenuPanelItem, PoMenuPanelModule, PoNotificationService, PoPageAction, PoPageModule, PoTableColumn, PoTableModule, PoTabsModule, PoToolbarModule } from '@po-ui/ng-components';
+import {
+  PoButtonModule,
+  PoComboFilterMode,
+  PoDividerModule,
+  PoFieldModule,
+  PoImageModule,
+  PoLoadingModule,
+  PoMenuPanelItem,
+  PoMenuPanelModule,
+  PoNotificationService,
+  PoPageAction,
+  PoPageModule,
+  PoTableAction,
+  PoTableColumn,
+  PoTableModule,
+  PoTabsModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
 import { APP_VERSION } from '../../version';
 import { ApiProtheusService } from './api-protheus.service.service';
-
 
 @Component({
   selector: 'app-rastreabilidade-op-vinculo-documento',
@@ -24,7 +40,7 @@ import { ApiProtheusService } from './api-protheus.service.service';
     FormsModule,
   ],
   templateUrl: './rastreabilidade-op-vinculo-documento.component.html',
-  styleUrl: './rastreabilidade-op-vinculo-documento.component.css'
+  styleUrl: './rastreabilidade-op-vinculo-documento.component.css',
 })
 export class RastreabilidadeOpVinculoDocumentoComponent implements OnInit {
   public menuItems: Array<PoMenuPanelItem> = []; //Definido no OnInit()
@@ -33,28 +49,39 @@ export class RastreabilidadeOpVinculoDocumentoComponent implements OnInit {
   isHideLoading: boolean = true;
   textLoadingOverlay: string = '';
   comboItems: Array<{ label: string; value: any }> = [];
+  customerOptions: Array<{ label: string; value: any }> = [];
   selectedOP: any;
+  selectedCustomer: any;
   opFilterMode = PoComboFilterMode.contains;
   mrTitulo: any;
   mrNomeEmpresa = sessionStorage.getItem('mrNomeEmpresa');
   mrDataBase = sessionStorage.getItem('mrDataBase');
   mrUsuario = sessionStorage.getItem('mrUsuario');
-  documentColumns: Array<PoTableColumn> = [
-    { property: 'branch', label: 'Filial', width: '8%' },
-    { property: 'quotationNumber', label: 'Nro Orçamento', width: '8%' },
-    { property: 'quotationItem', label: 'Item Orçamento', width: '8%' },
-    { property: 'salesOrderNumber', label: 'Nro Pedido', width: '8%' },
-    { property: 'salesOrderItem', label: 'Item Pedido', width: '8%' },
-    { property: 'quantity', label: 'Quantidade', width: '8%' },
+  poColumns: Array<PoTableColumn> = [
+    { property: 'branch', label: 'Filial', width: '6%' },
+    { property: 'productionOrderId', label: 'Nro Ordem', width: '12%' },
+    { property: 'productCode', label: 'Produto', width: '10%' },
+    { property: 'productDescription', label: 'Descrição', width: '35%' },
+    { property: 'issueDate', label: 'Emissão', width: '10%' },
+    { property: 'quantity', label: 'Quantidade', width: '10%' },
+    { property: 'linkedQuantity', label: 'Qtd vinculada', width: '10%' },
   ];
 
-  documentItems: Array<any> = [];
+  poItems: Array<any> = [];
+
+  poActions: PoTableAction[] = [
+    {
+      label: '',
+      icon: 'an an-plus-circle',
+      action: this.associateOrderProduction.bind(this),
+    },
+  ];
 
   constructor(
     private router: Router,
     private poNotification: PoNotificationService,
-    private ApiProtheusService: ApiProtheusService
-  ) { }
+    private ApiProtheusService: ApiProtheusService,
+  ) {}
 
   ngOnInit(): void {
     this.mrTitulo =
@@ -73,8 +100,9 @@ export class RastreabilidadeOpVinculoDocumentoComponent implements OnInit {
         icon: 'fa-solid fa-door-open',
       },
     ];
-
-    this.getLinkedOrders();
+    console.log('Iniciou o componente de vinculo OP x Documentos');
+    //this.getLinkedOrders();
+    this.getCustomers();
   }
 
   LogOff(menu: PoMenuPanelItem) {
@@ -104,9 +132,33 @@ export class RastreabilidadeOpVinculoDocumentoComponent implements OnInit {
     }
   }
 
-  async onPOSelect(opSelecionada: any) {
+  async getCustomers() {
+    console.log('executando getcustomers');
+    try {
+      this.textLoadingOverlay = 'Buscando Clientes';
+      this.isHideLoading = false;
 
-    const opItem = this.comboItems.find(item => item.value === opSelecionada);
+      let resposta = await this.ApiProtheusService.getCustomers();
+
+      // Supondo que resposta seja um array de objetos, e que você quer popular o combo com label/value.
+      // Ajuste os campos label e value conforme sua estrutura.
+      this.customerOptions = resposta.map((item) => ({
+        label: `${item.customerCode}-${item.customerBranch} ${item.customerName} (${item.cnpjNumber})`, //`${item.orderId} - Filial ${item.branch}`,
+        value: item.customerCode + item.customerBranch,
+      }));
+
+      this.isHideLoading = true;
+      this.textLoadingOverlay = 'Buscando Ops';
+    } catch (error) {
+      this.isHideLoading = true;
+      this.textLoadingOverlay = 'Buscando Ops';
+      //console.log(error);
+      this.poNotification.error('Erro ao buscar clientes.');
+    }
+  }
+
+  async onPOSelect(opSelecionada: any) {
+    const opItem = this.comboItems.find((item) => item.value === opSelecionada);
     this.selectedOP = opSelecionada;
 
     const branch = this.selectedOP.substring(0, 4);
@@ -118,18 +170,57 @@ export class RastreabilidadeOpVinculoDocumentoComponent implements OnInit {
       this.textLoadingOverlay = 'Buscando documentos';
       this.isHideLoading = false;
 
-      const result = await this.ApiProtheusService.getAvailableDocuments(branch, productionId);
+      const result = await this.ApiProtheusService.getAvailableDocuments(
+        branch,
+        productionId,
+      );
 
-      this.documentItems = result;
+      this.poItems = result;
       this.isHideLoading = true;
       this.textLoadingOverlay = '';
-
     } catch (error) {
       console.error('Erro ao vincular:', error);
       this.poNotification.error('Erro ao buscar documentos disponíveis.');
     } finally {
       this.isHideLoading = true;
     }
+  }
 
+  async onCustomerSelect(selectedCustomer: any) {
+    const opItem = this.comboItems.find(
+      (item) => item.value === selectedCustomer,
+    );
+    this.selectedCustomer = selectedCustomer;
+
+    const customerCode = this.selectedCustomer.substring(0, 6);
+    const customerBranch = this.selectedCustomer.substring(6, 8);
+    console.log(`this.selectedCustomer = ${this.selectedCustomer}`);
+    console.log(`customerCode = ${customerCode}`);
+    console.log(`customerBranch = ${customerBranch}`);
+    try {
+      this.textLoadingOverlay = 'Buscando OPs ...';
+      this.isHideLoading = false;
+
+      const result: any =
+        await this.ApiProtheusService.getAvailableProductionsOrders(
+          customerCode,
+          customerBranch,
+        );
+      console.log('result = ');
+      console.log(result);
+      this.poItems = result;
+      this.isHideLoading = true;
+      this.textLoadingOverlay = '';
+    } catch (error) {
+      console.error('Erro ao vincular:', error);
+      this.poNotification.error('Erro ao buscar ordens disponíveis.');
+    } finally {
+      this.isHideLoading = true;
+    }
+  }
+
+  async associateOrderProduction(row: any) {
+    console.log('Associando OP de produção ao documento ...');
+    console.log(row);
   }
 }
